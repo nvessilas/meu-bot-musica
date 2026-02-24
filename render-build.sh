73b06735e679198f75e0ec01087f4d6ab4b08273
nvessilas <86134153+nvessilas@users.noreply.github.com>
2026-02-23 21:11:38 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-# Instala o ffmpeg no servidor do Render
-apt-get update && apt-get install -y ffmpeg
+set -e
+
+# Baixa o FFmpeg portátil para Linux (não precisa de root)
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+tar -xf ffmpeg-release-amd64-static.tar.xz
+mv ffmpeg-*-amd64-static/ffmpeg .
+rm -rf ffmpeg-*-amd64-static* ffmpeg-release-amd64-static.tar.xz
+
 # Instala as bibliotecas do Python
 pip install -r requirements.txt
